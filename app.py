1c7c933b186ebbf80701b07367e835f7d4a888f4
egemen9000 <secimid2@gmail.com>
2026-02-26 17:17:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -91,9 +91,9 @@ if os.path.exists("canli_veriler.csv"):
 
         sinyal = df[
             (df['RSI7'] > df['RSI14']) &
-            (df['RSI14'] < 30) #&
-            #(df['Fiyat'] > df['VWAP']) &
-            #(df['Hacim'] > 2 * ortalama_hacim)
+            (df['RSI14'] < 30) &
+            (df['Fiyat'] > df['VWAP']) &
+            (df['Hacim'] > 2 * ortalama_hacim)
         ].copy()
 
         if not sinyal.empty:
@@ -117,5 +117,6 @@ if os.path.exists("canli_veriler.csv"):
 
 else:
     st.warning("Veritabanı boş. Lütfen yukarıdaki butona basarak verileri çekin.")
+
 
 
